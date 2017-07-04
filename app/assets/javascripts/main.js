10d58b2e604ae456b6f0b57167cb3d49ccc87b5b
swojeet <swojeetkayastha@gmail.com>
2017-07-04 20:50:00 +0545

    Display poster image too

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,46 @@
 $(function(){
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie?',
-  	data:  {"api_key" : "6187f5c607108a1517d59f4dae103bf7", "query" : "Jack+Reacher"}
-  })
-  .done(function(data){
-    displayMovies(data);
+  let form=$('#movie-search');
+  form.submit(function(e){
+    e.preventDefault();
+
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie?api_key=ab3927766ef5b5254ec77d1289540da8',
+      data: form.serialize()
+    })
+    .done(function(data){
+      displayMovies(data);
+    });
   });
+
 
   function displayMovies(data){
     let htmlString = "";
-
+    var imageUrl = getBaseImageUrl();
     data["results"].forEach(function(movie){
-      htmlString += `<p>${movie["title"]}</p>
+      htmlString += `<img src="${imageUrl}/${movie["poster_path"]}" />
+                    <p>${movie["title"]}</p>
                     <p>${movie["overview"]}</p>
                     `
     });
 
     $("#movies").append(htmlString);
   }
-})
+
+  function getBaseImageUrl(){
+    var url = "";
+    var settings = {
+      "async": false,
+      "crossDomain": true,
+      "url": "https://api.themoviedb.org/3/configuration?api_key=ab3927766ef5b5254ec77d1289540da8",
+      "method": "GET",
+      "headers": {},
+      "data": "{}"
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      url = response["images"]["base_url"] + response["images"]["poster_sizes"][2];
+    });
+    return url;
+  }
+});
