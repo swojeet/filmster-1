181869444e61c900c12edd7e481356305f8147bd
swojeet <swojeetkayastha@gmail.com>
2017-07-05 16:40:50 +0545

    Remove unnecessary code from movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,20 +1,3 @@
 class Movie < ApplicationRecord
   validates :title, :plot, :release_date, :released, :runtime, :popularity, :genre, :language, :budget, :average_vote, :vote_count, :poster, :homepage, :tmdb_id, :imdb_id, presence: true
 end
-
-
-t.string :title
-t.string :plot
-t.string :release_date
-t.boolean :released, null: false, default: false
-t.string :runtime
-t.string :popularity
-t.string :genre
-t.string :language
-t.string :budget
-t.string :average_vote
-t.string :vote_count
-t.string :poster
-t.string :homepage
-t.string :tmdb_id
-t.string :imdb_id
