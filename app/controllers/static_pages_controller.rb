fa7e39d604f5639cdb9959e5121546cda50c4530
swojeet <swojeetkayastha@gmail.com>
2017-07-17 17:00:28 +0545

    Show recent reviews in the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def index
     @movies = Tmdb::Movie.top_rated
     @movies.results.each { |movie| MovieBuilder.new(tmdb_id: movie.id).build!}
+
+    @recent_reviews = Review.recent
   end
 
   def timeline
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,11 @@ class Review < ApplicationRecord
   belongs_to :movie
 
   delegate :username, to: :user,prefix: true
+  delegate :title, :tmdb_id, to: :movie, prefix: true
 
   validates :user, :movie, presence: true
 
   has_many :upvotes, dependent: :destroy
+
+  scope :recent, -> { order("created_at DESC").limit(5) }
 end
