816763d6a50c36d1743d7832adc90431c1b737ab
Aadesh <aadeshere1@gmail.com>
2017-07-31 15:23:10 +0545

    change relationship migration

--- db/migrate/20170713164156_create_relationships.rb
+++ db/migrate/20170713164156_create_relationships.rb
@@ -1,10 +1,13 @@
 class CreateRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :relationships do |t|
-      t.references :follower, foreign_key: true
-      t.references :followed, foreign_key: true
+      t.integer :follower_id
+      t.integer :followed_id
 
       t.timestamps
     end
+
+    add_index :relationships, :followed_id
+    add_index :relationships, :follower_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 20170715093048) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "movie_id"
+    t.bigint "user_id"
+    t.bigint "movie_id"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 20170715093048) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
   add_foreign_key "upvotes", "reviews"
   add_foreign_key "upvotes", "users"
 end
